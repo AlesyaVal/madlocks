bfbdf4c36be04dfdaaebea034ee542434333946a
Viktoryia <maltsevago@mail.ru>
2018-10-24 17:43:33 +0300

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="ru">
+ <head>
+   <meta charset="utf-8">
+   <meta name="description" content="1, 2, 3 предложения">
+   <meta name="keywords" content="выражения через ,">
+   <meta name="author" content="">
+   <title> название сайта </title>
+   <link  type="text/css" rel="stylesheet" href="/public/media/bootstrap/css/bootstrap.min.css"/>
+   <link  type="text/css" rel="stylesheet" href="/public/media/css/style.css"/>
+   <script src="https://vk.com/js/api/openapi.js?159" type="text/javascript"></script>
+   <head>
+   	<body>
+   		<header id="header">
+   		 <h1 id="logotext"> MadLocks </h1>	
+   		 <img id="logo" src="/public/media/img/logo.png"/>
+   		</header>
+   		<nav class="topmenu">
+   		<a href="#">Главная</a>
+   		<a href="#">Наши мастера</a>
+   		<a href="#">Услуги</a>
+   		<a href="#">Карта проезда</a>
+   		<a href="#">Контакты</a>	
+   		</nav>
+   		<main>
+   		 <div>
+   		  <aside class="col-md-2">
+   		  <a href="#" class="btn btn-default btn-block">Галерея</a> 
+   		  <a href="#" class="btn btn-default btn-block">Прайс-лист</a>   
+   		  <a href="#" class="btn btn-default btn-block">Отзывы</a> 			
+		  </aside>
+   		<article class="col-md-8"> 
+   			<h2>Добро пожаловать на наш сайт</h2>
+   			<div class="maintext"></div>
+   			<p class="lead">
+   		
+			<p style="margin-left: 40px;"><em>AFROMARI -
+это не просто студия по плетению афрокосичек, это настоящая мастерская для твоего нового,необычного образа! Студия основанаМарией Борисовой -
+мастером плетения с огромным теоретическими практическим  опытом. В студии заплетено уже более 17000 косичек,мастера обучались у лучших брейдистов России и работают только с качественными материалами!</em></p><p>
+	<p style="text-align: center;"><img alt="Lorem Ipsum" src="/public/media/img/1.jpg" style="width: 600px; height: 550px; border-width: 1px; border-style: solid;"></p><p style="margin-left: 40px;"></p><p>
+	<p style="margin-left: 40px;"><em>AFROMARI -
+это не просто студия по плетению афрокосичек, это настоящая мастерская для твоего нового,необычного образа! Студия основанаМарией Борисовой -
+мастером плетения с огромным теоретическими практическим  опытом. В студии заплетено уже более 17000 косичек,мастера обучались у лучших брейдистов России и работают только с качественными материалами!</em></p><p>
+   
+         <div class="node node472 section section-clear" id="b" data-opacity="0.95"><div class="wrapper1"><div class="wrapper2"><div class="container"><div class="cont"><div class="node node473 widget widget-text"><div class="wrapper1"><div class="wrapper2"><div class="xs-scale-80"><h2 class="font-header spans xs-force-center textable"><span style="font-size: 34px; letter-spacing: 0.1em; text-align: center;" class="p"><span style="color: rgb(0, 0, 0);"><span style="font-size: 48px;"><strong>Вопросы </strong></span></span></span></h2></div></div></div></div><div class="node node474 widget widget-text"><div class="wrapper1"><div class="wrapper2"><div class="flex"><div style="opacity: 0.5;" class="xs-force-center textable"><p style="font-size: 110%; text-align: center;"><span style="color: rgb(17, 17, 17);">И наши ответы</span></p></div></div></div></div></div><div class="node node475 widget widget-blank"><div class="wrapper1"><div class="wrapper2"><div class="padding"></div></div></div></div><div class="node node476 widget widget-spoiler"><div class="wrapper1"><div class="wrapper2"><div class="metahtml"><div id="node476_meta" class="code"><div class="spoiler2">
+  <div plp-spoiler-toggle="" il-action="toggle" class="spoiler2-header is-expanded">
+    <div class="textable"><p style="letter-spacing: 0.1em;">ВРЕДНЫ ЛИ АФРОКОСЫ <span style="font-size: 14px;">?</span></p></div>
+    <div class="spoiler2-toggle">
+      <i class="fa fa-chevron-down"></i>
+    </div>
+  </div>
+  <div style="display: block;" plp-spoiler-container="" class="spoiler2-container is-expanded">
+    <div class="cont"><div class="node node477 widget widget-text"><div class="wrapper1"><div class="wrapper2"><div class=""><div class="textable"><p style="line-height: 1.5;">&nbsp;До сих пор можно услышать, что афроплетение вредно для собственных волос.&nbsp;</p><p style="line-height: 1.5;">Аргументируется это значительным «вычесом» волос, который отмечается после расплетения косичек. Опровергнуть это предубеждение довольно просто.<br>По естественным причинам у любого человека каждый день выпадает около 100 волосков. На их месте вырастают новые, и объем прически не меняется.<br>Афрокосички создаются на срок от 1 месяца. Путем нехитрых подсчетов выясняем, что за это время у человека выпадает минимум 3000 волос. Вот именно их и вычесывают после расплетания косичек. Поэтому волноваться не стоит, потому что за это время выросло такое же количество новых волосков! К тому же &nbsp;волосы отдохнут от укладок, фенов и воздействия агрессивной среды.<br>А при правильном уходе за косами, они точно не принесут вам никакого вреда!</p><p style="line-height: 1.5;"><br>Носите косы с удовольствием!)</p></div></div></div></div></div></div>
+  </div>
+</div></div></div></div></div></div>
+<p style="text-align: center;"><img alt="Lorem Ipsum" src="/public/media/img/2.jpg" style="width: 600px; height: 550px; border-width: 1px; border-style: solid;"></p><p style="margin-left: 40px;"></p><p><div class="node node478 widget widget-blank"><div class="wrapper1"><div class="wrapper2"><div class="padding"></div></div></div></div><div class="node node479 widget widget-spoiler"><div class="wrapper1"><div class="wrapper2"><div class="metahtml"><div id="node479_meta" class="code"><div class="spoiler2">
+  <div plp-spoiler-toggle="" il-action="toggle" class="spoiler2-header is-expanded">
+    <div class="textable"><p style="letter-spacing: 0.1em;">еСТЬ ЛИ ПРОТИВОПОКАЗАНИЯ <span style="font-size: 14px;">?</span></p></div>
+    <div class="spoiler2-toggle">
+      <i class="fa fa-chevron-down"></i>
+    </div>
+  </div>
+  <div style="display: block;" plp-spoiler-container="" class="spoiler2-container is-expanded">
+    <div class="cont"><div class="node node480 widget widget-text"><div class="wrapper1"><div class="wrapper2"><div class=""><div class="textable"><p style="line-height: 1.5;">&nbsp;Да, &nbsp;ни в коем случае не делай АФРОКОСЫ, если у тебя:</p><p style="line-height: 1.5;"><br>Вегетососудистая дистония, частные головные боли, мигрени&nbsp;</p><p style="line-height: 1.5;">Раны, ссадины, незажившие шрамы на голове&nbsp;</p><p style="line-height: 1.5;">Хронические кожные заболевания, нейродермит, псориаз&nbsp;</p><p style="line-height: 1.5;">Себорея&nbsp;</p><p style="line-height: 1.5;">Выпадение волос, облысение</p><p style="line-height: 1.5;"><br>В этих случаях афрокосы только усугубят твоё положение, береги себя и мир вокруг 💕&nbsp;</p></div></div></div></div></div></div>
+  </div>
+</div></div></div></div></div></div>
+<p style="text-align: center;"><img alt="Lorem Ipsum" src="/public/media/img/3.jpg" style="width: 600px; height: 550px; border-width: 1px; border-style: solid;"></p><p style="margin-left: 40px;"></p><p><div class="node node481 widget widget-blank"><div class="wrapper1"><div class="wrapper2"><div class="padding"></div></div></div></div><div class="node node482 widget widget-spoiler"><div class="wrapper1"><div class="wrapper2"><div class="metahtml"><div id="node482_meta" class="code"><div class="spoiler2">
+  <div plp-spoiler-toggle="" il-action="toggle" class="spoiler2-header is-expanded">
+    <div class="textable"><p style="letter-spacing: 0.1em;"><span style="font-size: 14px;">КАК УХАЖИВАТЬ ЗА АФРОКОСАМИ ?</span></p></div>
+    <div class="spoiler2-toggle">
+      <i class="fa fa-chevron-down"></i>
+    </div>
+  </div>
+  <div style="display: block;" plp-spoiler-container="" class="spoiler2-container is-expanded">
+    <div class="cont"><div class="node node483 widget widget-text"><div class="wrapper1"><div class="wrapper2"><div class=""><div class="textable"><p style="line-height: 1.5;"><span title="Отредактировано">Косички-самая неприхотливая прическа</span></p><p style="line-height: 1.5;"><span title="Отредактировано"><br>Они не требуют никакого специального ухода,но есть пару рекомендаций<br>Перед сном сплетите все косички в одну большую кому, так они меньше будут пушиться и сохранят дольше свой эстетический вид<br>Так же перед сном можно одеть платочек на голову, что бы у корней не выбивались пушковые волосы<br>Если вас смущают торчащие волоски из косичек, то тут только один способ, подрежте их, кстати мы тоже это делаем по вашему желанию когда заплетаем вас<br>Ну и конечно своевременная коррекция теменной зоны и окантовки поможет вам проходить с косами как можно дольше&nbsp;</span></p></div></div></div></div></div></div>
+  </div>
+</div></div></div></div></div></div><div class="node node484 widget widget-blank"><div class="wrapper1"><div class="wrapper2"><div class="padding"></div></div></div></div><div class="node node485 widget widget-spoiler"><div class="wrapper1"><div class="wrapper2"><div class="metahtml"><div id="node485_meta" class="code"><div class="spoiler2">
+  <div plp-spoiler-toggle="" il-action="toggle" class="spoiler2-header is-expanded">
+    <div class="textable"><p style="letter-spacing: 0.1em;"><span style="font-size: 14px;">Как мыть афрокосы?<br></span></p></div>
+    <div class="spoiler2-toggle">
+      <i class="fa fa-chevron-down"></i>
+    </div>
+  </div>
+  <div style="display: block;" plp-spoiler-container="" class="spoiler2-container is-expanded">
+    <div class="cont"><div class="node node486 widget widget-text"><div class="wrapper1"><div class="wrapper2"><div class=""><div class="textable"><p style="line-height: 1.5;"><span title="Отредактировано">Этим вопросом задаются все, даже тот кто не собирается их себе плести<br></span></p><p style="line-height: 1.5;"><span title="Отредактировано">Моют афрокосички раз в неделю теплой водой с обычным шампунем</span></p><p style="line-height: 1.5;"><span title="Отредактировано"><br><span title="Отредактировано">•</span> Слишком часто мыть не стоит – от этого внешний вид только ухудшится!<br><br>Лучше всего подойдет шампунь для нормальных волос, без содержания бальзамов и кондиционеров.&nbsp;</span></p><p style="line-height: 1.5;"><span title="Отредактировано">Нужно немного шампуня нанести на губку, вспенить и промыть кожу головы (вся пена которая будет стекать по косам, соберёт с них всю грязь, отдельно косы мыть не нужно)</span></p><p style="line-height: 1.5;"><span title="Отредактировано"><br>!Важно тщательно смывать остатки шампуня!</span></p><p style="line-height: 1.5;"><span title="Отредактировано"><br><span title="Отредактировано">•</span> Нельзя использовать масла, бальзамы и кондиционеры, так как они тяжело вымываются и только лишь утяжеляют наши косы!</span></p><p style="line-height: 1.5;"><span title="Отредактировано"><br>После слегка отжать косички и завернуть в полотенце, чтобы оно впитало остальную влагу.&nbsp;</span></p><p style="line-height: 1.5;"><span title="Отредактировано">Досушивают голову при комнатной температуре обычным способом•<br></span></p><p style="line-height: 1.5;"><span title="Отредактировано">Можно высушить феном, ! только не рекомендуется использовать горячий воздух ! , так как он может повредить синтетический материал, лучше прохладный или теплый</span></p><p style="line-height: 1.5;"><span title="Отредактировано"><br><span title="Отредактировано">•</span>Также посещать баню, сауну и находиться под палящим солнцем нужно в головном уборе.<br></span></p></div></div></div></div></div></div>
+  </div>
+</div></div></div></div></div></div><div class="node node487 widget widget-blank"><div class="wrapper1"><div class="wrapper2"><div class="padding"></div></div></div></div><div class="node node488 widget widget-blank"><div class="wrapper1"><div class="wrapper2"><div class="padding"></div></div></div></div><div class="node node489 widget widget-spoiler"><div class="wrapper1"><div class="wrapper2"><div class="metahtml"><div id="node489_meta" class="code"><div class="spoiler2">
+  <div plp-spoiler-toggle="" il-action="toggle" class="spoiler2-header is-collapsed">
+    <div class="textable"><p style="letter-spacing: 0.1em;">как расплетаются афрокосы<span style="font-size: 14px;">?</span></p></div>
+    <div class="spoiler2-toggle">
+      <i class="fa fa-chevron-down"></i>
+    </div>
+  </div>
+  <div style="display: none;" plp-spoiler-container="" class="spoiler2-container is-collapsed">
+    <div class="cont"><div class="node node490 widget widget-text"><div class="wrapper1"><div class="wrapper2"><div class=""><div class="textable"><p style="line-height: 1.5;">Есть два варианта:</p><p style="line-height: 1.5;"><br>1️.Вы приезжаете к нам в студию и снимаем вам Косички, просто расплетая их, а дальше уже даем ЦУ<br>Процесс это не быстрый, занимает примерно столько же времени сколько и плетение, поэтому и цена соответствующая&nbsp;</p><p style="line-height: 1.5;"><br>2.Вы сами садитесь одна, либо с подружками и расплетаете чудесные Косички.Расплетаются они очень легко, если это Зизи, то узелок развязывается, либо срезается и дальше как простая косичка все расплетается, если это афро с канекалоном, то тут вы отстригаете косу до ваших волос и тоже дальше расплетаете Косички .</p></div></div></div></div></div></div>
+  </div>
+</div></div></div></div></div></div></div></div><span class="plplink"></span></div></div></article>
+
+            </p>
+			
+
+   			</p>
+   		 </main>
+   		<aside class="col-md-2"> 
+        
+        <aside id="dev_widget_preview" class="dev_widget_preview" style="width: 300px; height: 280px; background: none;"><iframe name="fXDc71ab" frameborder="0" src="https://vk.com/widget_community.php?app=1936057&amp;width=200px&amp;_ver=1&amp;gid=34188066&amp;mode=3&amp;color1=&amp;color2=&amp;color3=&amp;class_name=&amp;url=https%3A%2F%2Fvk.com%2Fdev%2FCommunity%3Fheight%3D400%26link%3Dhttps%253A%252F%252Fvk.com%252Fcreative_studio_jh%26mode%3D3%26no_cover%3D%26oid%3D-34188066%26wide%3D&amp;referrer=https%3A%2F%2Fwww.kasper.by%2F&amp;title=%D0%92%D0%B8%D0%B4%D0%B6%D0%B5%D1%82%20%D0%B4%D0%BB%D1%8F%20%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D1%81%D1%82%D0%B2%20%7C%20%D0%A0%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%B0%D0%BC&amp;166a2723370" width="200" height="185" scrolling="no" id="vkwidget16" style="overflow: hidden; height: 190px;"></iframe></aside>
+
+   		</aside>
+   		<br style="clear:both"/>
+   		<footer class="footer">
+   			2018 &copy; AlesyaValko
+   		</footer>
+   	  
+   	</body>
+   </head>
